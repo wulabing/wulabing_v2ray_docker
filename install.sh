ff6e6ac80a9eb9370e7c724d0209874b404190c3
wulabing <wulabing@gmail.com>
2021-12-06 18:47:28 +0800

    v0.1
    bug fix

--- install.sh
+++ install.sh
@@ -204,7 +204,7 @@ modify_nginx_port() {
 modify_nginx_other() {
   sed -i "/server_name/c \\\tserver_name ${domain};" ${nginx_conf}
   sed -i "/location/c \\\tlocation ${camouflage}" ${nginx_conf}
-  sed -i "/proxy_pass/c \\\tproxy_pass http://127.0.0.1:${PORT};" ${nginx_conf}
+  sed -i "/proxy_pass/c \\\tproxy_pass http://v2ray:${PORT};" ${nginx_conf}
   sed -i "/return/c \\\treturn 301 https://${domain}\$request_uri;" ${nginx_conf}
   #sed -i "27i \\\tproxy_intercept_errors on;"  ${nginx_dir}/conf/nginx.conf
 }
@@ -212,7 +212,7 @@ web_camouflage() {
   ##请注意 这里和LNMP脚本的默认路径冲突，千万不要在安装了LNMP的环境下使用本脚本，否则后果自负
   rm -rf $web_dir
   mkdir -p $web_dir
-  git clone https://github.com/wulabing/3DCEList.git
+  git clone https://github.com/wulabing/3DCEList.git $web_dir
   judge "web 站点伪装"
 }
 nginx_pre_install() {
@@ -306,7 +306,7 @@ nginx_conf_add() {
         ssl_ciphers           TLS13-AES-256-GCM-SHA384:TLS13-CHACHA20-POLY1305-SHA256:TLS13-AES-128-GCM-SHA256:TLS13-AES-128-CCM-8-SHA256:TLS13-AES-128-CCM-SHA256:EECDH+CHACHA20:EECDH+CHACHA20-draft:EECDH+ECDSA+AES128:EECDH+aRSA+AES128:RSA+AES128:EECDH+ECDSA+AES256:EECDH+aRSA+AES256:RSA+AES256:EECDH+ECDSA+3DES:EECDH+aRSA+3DES:RSA+3DES:!MD5;
         server_name           serveraddr.com;
         index index.html index.htm;
-        root  /www/3DCEList;
+        root  /www;
         error_page 400 = /400.html;
 
         # Config for 0-RTT in TLSv1.3
@@ -319,7 +319,7 @@ nginx_conf_add() {
         {
         proxy_redirect off;
         proxy_read_timeout 1200s;
-        proxy_pass http://127.0.0.1:10000;
+        proxy_pass http://v2ray:10000;
         proxy_http_version 1.1;
         proxy_set_header X-Real-IP \$remote_addr;
         proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
